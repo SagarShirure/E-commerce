a07ace5e66d52a7308c9b6fd22d2ff10091d3c40
Sagar Devidas Shirure <sagarshirure145@gmail.com>
2024-08-10 14:13:07 +0530

    E-Commerce

---
+++ src/customer/component/Home/MainCarouselData.jsx
@@ -0,0 +1,19 @@
+export const mainCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_15.jpg",
+        // images:"https://www.ethnicplus.in/lehenga-choli",
+        path:"./2_15.jpg"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_15.jpg",
+        path:"./3_17.jpg"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/5/_/5_11.jpg",
+        path:"./4_15.jpg"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_15.jpg",
+        path:"5_11.jpg"
+    }
+]
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainCarousel from '../../component/Home/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <div>
+            <MainCarousel/>
+        </div>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
